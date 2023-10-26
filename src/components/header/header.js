db05ff8125091bf47f73103dca4fe97020c83927
abramowsergey1992 <71379145+abramowsergey1992@users.noreply.github.com>
2023-10-26 06:45:12 +0500

    header refactoring

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,35 +4,35 @@ let overlay = document.querySelector(".header__overlay");
 let header = document.querySelector(".header");
 let burger = document.querySelector(".header__burger");
 
-[].forEach.call(
-    document.querySelectorAll(".main-menu__sub-back"),
-    function (el) {
-        el.addEventListener("click", function (event) {
-            document
-                .querySelector(".main-menu__sub._active")
-                .classList.remove("_active");
-        });
-    }
-);
-[].forEach.call(links, function (el) {
+document.querySelectorAll(".js-close-menu").forEach((el) => {
+    el.addEventListener("click", function (event) {
+        document
+            .querySelector(".main-menu__sub._active")
+            .classList.remove("_active");
+    });
+});
+links.forEach((el) => {
     el.addEventListener("click", function (event) {
         event.preventDefault();
-        [].forEach.call(links, function (el) {
+        links.forEach((el) => {
             el.classList.remove("_active");
         });
-        [].forEach.call(submenu, function (el) {
+        submenu.forEach((el) => {
             el.classList.remove("_active");
         });
         el.classList.add("_active");
         header.classList.add("_overlay-visible");
         document.querySelector(el.dataset.sub).classList.add("_active");
-        overlay.addEventListener("click", function () {
-            header.classList.remove("_overlay-visible");
-        });
     });
 });
-
+overlay.addEventListener("click", function () {
+    header.classList.remove("_overlay-visible");
+    document.body.classList.remove("_burger-menu-open");
+    document
+        .querySelector(".main-menu__sub._active")
+        .classList.remove("_active");
+});
 burger.addEventListener("click", function (event) {
-    document.body.classList.toggle("_burder-menu-open");
+    document.body.classList.toggle("_burger-menu-open");
 });
 
