43604a4543f3922f10df366b02667eb71251181a
Sergey <geminic@yandex.ru>
2023-11-09 14:55:42 +0400

    Add block program

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -9,3 +9,20 @@ document.querySelectorAll('.js-hp-video').forEach(el => {
         container.querySelector('video')?.play();
     }
 });
+
+
+// Hovers with images
+if (!GLOBALS.mobile) {
+    document.querySelectorAll('.js-hp-hover-btn').forEach(btn => {
+        const id = btn.dataset.id;
+        const img = btn.closest('.js-hp-hover')?.querySelector(`.js-hp-hover-bg[data-id="${id}"]`);
+        
+        btn.addEventListener('mouseenter', () => {
+            img?.classList.add('_is-visible');
+        });
+
+        btn.addEventListener('mouseleave', () => {
+            img?.classList.remove('_is-visible');
+        });
+    });
+}
